46bd8585e7e54b7f3758b34b2eaa45845c627ebc
bernardo damiani <bmdamiani17@gmail.com>
2024-08-23 16:55:35 -0300

    chore (tailwind config): extend colors and screens styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         red: {
-          100: '#F15156',
-          200: '#E44449',
+          100: "#F15156",
+          200: "#E44449",
         },
-        yellow: '#F4D35E',
-        blue: '#0D3B66',
+        yellow: "#F4D35E",
+        blue: "#0D3B66",
+        input: {
+          100: "#F5F8FA",
+          200: "#D3E2E5",
+        },
       },
       screens: {
-        xs: '475px',
+        xs: "475px",
       },
     },
   },
