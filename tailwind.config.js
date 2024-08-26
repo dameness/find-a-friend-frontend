b0368df20365414eafc4cb0017959824d5193dcb
bernardo damiani <bmdamiani17@gmail.com>
2024-08-26 15:47:46 -0300

    chore: add assets and extend tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,17 @@ export default {
     extend: {
       colors: {
         red: {
+          50: "#F75F64",
           100: "#F15156",
           200: "#E44449",
         },
         yellow: "#F4D35E",
-        blue: "#0D3B66",
+        orange: "#F27006",
+        green: "#3CDC8C",
+        blue: {
+          100: "#114A80",
+          200: "#0D3B66",
+        },
         input: {
           100: "#F5F8FA",
           200: "#D3E2E5",
